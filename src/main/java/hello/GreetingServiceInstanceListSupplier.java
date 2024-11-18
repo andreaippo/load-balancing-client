68825bffdbf9a2edf736f2ee3da7616d5c0822dc
Andrea Ippolito <andrea.ippo@gmail.com>
2024-11-18 17:56:02 +0100

    Simulate variable error rate from downstream services

--- src/main/java/hello/GreetingServiceInstanceListSupplier.java
+++ src/main/java/hello/GreetingServiceInstanceListSupplier.java
@@ -1,20 +1,24 @@
 package hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.DefaultServiceInstance;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
 import reactor.core.publisher.Flux;
 
-import java.util.Arrays;
 import java.util.List;
 
 class GreetingServiceInstanceListSupplier implements ServiceInstanceListSupplier {
 
+  private static final Logger log = LoggerFactory.getLogger(GreetingServiceInstanceListSupplier.class);
   private final String serviceId;
 
   @Value("${greeting-service-host}")
   private String host;
+
+  private long hits;
 
   GreetingServiceInstanceListSupplier(String serviceId) {
     this.serviceId = serviceId;
@@ -27,8 +31,13 @@ class GreetingServiceInstanceListSupplier implements ServiceInstanceListSupplier
 
   @Override
   public Flux<List<ServiceInstance>> get() {
-    return Flux.just(Arrays.asList(new DefaultServiceInstance(serviceId + "1", serviceId, host, 8090, false),
-      new DefaultServiceInstance(serviceId + "2", serviceId, host, 8091, false),
-      new DefaultServiceInstance(serviceId + "3", serviceId, host, 8092, false)));
+    hits++;
+    log.info("hits: {}", hits);
+    // simulate a cyclic error rate of 30% by routing to unreachable hosts
+    if (hits % 100 > 70) {
+      return Flux.just(List.of(new DefaultServiceInstance(serviceId + "2", serviceId, host, 8091, false),
+        new DefaultServiceInstance(serviceId + "3", serviceId, host, 8092, false)));
+    }
+    return Flux.just(List.of(new DefaultServiceInstance(serviceId + "1", serviceId, host, 8090, false)));
   }
 }
